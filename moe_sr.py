33e5c2a17c5e01bd8b1f3e7768731ab6c8dfdf3a
PuppyOne <zheng_xz@qq.com>
2025-07-22 08:50:11 +0800

    refactor(server): minor code cleanup
    - Remove unused imports and variables
    - Simplify condition checks

--- moe_sr.py
+++ moe_sr.py
@@ -1,20 +1,16 @@
-import math
 import os
-from threading import Thread
+import math
 import traceback
 from pathlib import Path
-import json
-from typing import Any
-from uuid import UUID, uuid4
+from typing import Literal
+from uuid import uuid4
 
 from flask import Flask, request
-from flask_cors import CORS
+from werkzeug.utils import secure_filename
 import numpy as np
 import cv2
 
 from onnx_infer import OnnxSRInfer
-
-from werkzeug.utils import secure_filename
 
 class ModelInfo:
     def __init__(self, name, path, scale, algo):
@@ -23,49 +19,38 @@ class ModelInfo:
         self.scale = scale
         self.algo = algo
 
+State = Literal['idle', 'processing', 'finish', 'error', 'cancel']
+
 
 # Global Vars
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 MAX_FILE_SIZE = 10 * 1024 * 1024  # 10MB
-BASE_URL = os.getenv('BASE_URL', 'http://localhost:9000')
 gpuid = 0
 tileSize = 192
 inputType= 'Image'
-model_list = []
 
-def get_base_path():
-    """从环境变量获取挂载点"""
-    oss_path = os.getenv('BASE_PATH')
-    if oss_path:
-        return Path(oss_path)
-    return Path.cwd()  # 默认使用当前工作目录
+# From env
+base_url = os.getenv('BASE_URL', 'http://localhost:9000')
+base_path = Path(os.getenv('BASE_PATH','./'))
+is_production = os.getenv('production') == 'true'
 
-base_path = get_base_path()
-
-tasks: dict[UUID, dict[str, Any]] = {}
+last_state: State = 'idle'
 last_progress = None
 last_progress_set_time = None
 # Scan models
+model_list = []
 model_root = Path('models')
 for algo in ['real-esrgan', 'real-hatgan']:
     for folder in [p for p in (model_root / algo).iterdir() if p.is_dir()]:
         for f in folder.glob('*.onnx'):
             model_list.append(ModelInfo(str(f.stem), str(f), int(folder.stem.replace('x', '')), algo))
 
-# eel.init('webui', custom_js_func=['handleSetProgress', 'showError', 'handleSetProcessState'])
-# prepare electron app
-# main_js = open('electron_app/main.js')
-# main_js_str = main_js.read()
-# main_js.close()
-# main_js_str_custom_port = re.sub('http://localhost:.*/', f'http://localhost:{port}/', main_js_str)
-# main_js = open('electron_app/main.js', 'w', encoding='utf-8')
-# main_js.write(main_js_str_custom_port)
-# main_js.close()
 
-app= Flask(__name__)
-CORS(app)
+app = Flask( __name__)
+if not is_production:
+    from flask_cors import CORS
+    CORS(app)
 
-# @eel.expose
 @app.get('/model_list')
 def py_get_model_list():
     algo_name=request.args.get('algo')
@@ -75,19 +60,19 @@ def py_get_model_list():
     return models
 
 # @eel.expose
-def py_get_settings():
-    setting_file = open(base_path / 'settings.json', 'r', encoding='utf-8')
-    settings = json.load(setting_file)
-    setting_file.close()
-    return settings
+# def py_get_settings():
+#     setting_file = open('settings.json','r',encoding='utf-8')
+#     settings = json.load(setting_file)
+#     setting_file.close()
+#     return settings
 
 # @eel.expose
-def py_save_settings(new_settings):
-    setting_file = open(base_path / 'settings.json', 'w', encoding='utf-8')
-    settings = json.dumps(new_settings,ensure_ascii=False)
-    setting_file.write(settings)
-    setting_file.close()
-    return 0
+# def py_save_settings(new_settings):
+#     setting_file = open('settings.json','w',encoding='utf-8')
+#     settings = json.dumps(new_settings,ensure_ascii=False)
+#     setting_file.write(settings)
+#     setting_file.close()
+#     return 0
 
 def seconds_to_hms(seconds):
     hours = seconds // 3600
@@ -126,25 +111,22 @@ def show_error(error_text):
     print(f"Error: {error_text}")
 
 
-last_state = 'idle'
-def set_process_state(state):
+def set_process_state(state: State):
     # eel.handleSetProcessState(state)
-    print(f"Process State: {state}")
     global last_state
+    print(f"Process State: {state}")
     last_state = state
 
 
-# @eel.expose
 @app.post('/run_process')
 def py_run_process():
     if last_state == 'processing':
         return {'error': 'A process is already running.'}, 400
 
-    set_process_state('processing')
     # modelName, tileSize, scale, isSkipAlpha, resizeTo: str, inputType, inputImage, outputPath, gpuid,algoName
     # modelName = request.args.get('modelName')
     # tileSize = request.args.get('tileSize', type=int)
-    scale = request.form.get('scale', type=int)
+    scale = request.form.get('scale', 2, type=int)
     isSkipAlpha = request.form.get('isSkipAlpha', 'false').lower() == 'true'
     # resizeTo = request.args.get('resizeTo')
     # inputType = request.args.get('inputType')
@@ -152,16 +134,14 @@ def py_run_process():
     # outputPath = request.args.get('outputPath')
     # # gpuid = request.args.get('gpuid', '-1')
     # algoName = request.args.get('algoName')
+    
 
     model_param = request.form.get('model')
-    if model_param and '-' in model_param:
+    if model_param and ':' in model_param:
         algoName, modelName = model_param.split(':', 1)
-
-    # 检查必填参数
-    required_params = [modelName, tileSize, scale, inputType, algoName] # inputImage, outputPath,
-    if any(p is None or p == '' for p in required_params):
-        return {'error': 'Missing required query parameters.'}, 400
-
+    else:
+        return {'error': 'Invalid or missing model parameter.'}, 400 
+        
     # 检查请求中是否有文件部分
     if 'image' not in request.files:
         return {'error': 'No image part in request'}, 400
@@ -180,10 +160,8 @@ def py_run_process():
         return {'error': 'File size exceeds limit'}, 413
 
     try:
-
+        set_process_state('processing')
         inputImage,outputPath = upload_image(file)
-        print(f"Input image: {inputImage}, Output path: {outputPath}")
-
         # find model info
         model = ModelInfo('', '', 4, '')
         provider_options = None
@@ -199,21 +177,7 @@ def py_run_process():
         # skip alpha sr
         if isSkipAlpha:
             sr_instance.alpha_upsampler = 'interpolation'
-
-        # task_thread = Thread(
-        #     target=process_image,
-        #     args=(
-        #         task_id,
-        #         sr_instance,
-        #         inputImage,
-        #         outputPath,
-        #         tileSize,
-        #         scale,
-        #         model,
-        #         resizeTo,
-        #     ),
-        # )
-        # task_thread.start()
+        
         output_path = process_image(
             sr_instance,
             inputImage,
@@ -222,18 +186,16 @@ def py_run_process():
             scale,
             model,
         )
-
         # Ensure output_path is relative to base_path for correct URL
         rel_output_path = str(Path(output_path).relative_to(base_path))
         return {
             'status': 'success',
             'outputPath': output_path,
-            'outputUrl': f"{BASE_URL}/{rel_output_path.replace(os.sep, '/')}",
+            'outputUrl': f"{base_url}/{rel_output_path.replace(os.sep, '/')}",
             'modelName': model.name,
             'scale': model.scale,
             'algo': model.algo,
-        }, 201
-
+        }
         # batch process
         # imgs_in = []
         # if inputType == 'Folder':
@@ -243,7 +205,7 @@ def py_run_process():
         #     for f in input_folder.glob('*.png'):
         #         imgs_in.append(f)
         # else:
-        # imgs_in = [inputImage]
+        #     imgs_in = [inputImage]
         # sr_instance.total_img_num = len(imgs_in)
         # sr_instance.processed_img_num = 0
 
@@ -256,18 +218,18 @@ def py_run_process():
         }, 500
 
 def upload_image(file) -> tuple[Path, Path]:
-    # 生成唯一文件夹
+    # Generate a unique folder path
     unique_id = str(uuid4())
     folder_path = base_path / unique_id
     folder_path.mkdir(parents=True, exist_ok=True)
 
-    # 生成安全的文件名
+    # Generate input file name
     original_filename = secure_filename(file.filename)
     file_ext = original_filename.rsplit('.', 1)[1].lower()
     input_filename = f"input.{file_ext}"
     input_path = folder_path / input_filename
 
-    # 保存输入图片
+    # Save file
     file.save(input_path)
 
     return input_path, folder_path
@@ -284,6 +246,7 @@ def process_image(
 ) -> str:
     """sr process"""
     img_in=base_path / inputImage
+
     # for img_in in imgs_in:
     img = cv2.imdecode(np.fromfile(img_in,dtype=np.uint8),cv2.IMREAD_UNCHANGED)
     h, w, c = img.shape
@@ -317,7 +280,7 @@ def process_image(
             target_w = int(w * ratio)
             target_h = int(h * ratio)
     if target_w:
-        img_out = cv2.resize(sr_img, (target_w, target_h))
+        img_out = cv2.resize(sr_img, (target_w, target_h)) # type: ignore
     else:
         img_out = sr_img
     # save
@@ -350,14 +313,8 @@ def get_task_status(task_id: str):
         'last_progress': last_progress,
         'last_progress_set_time': last_progress_set_time,
     }
-
-
-# eel.start('index.html', mode='custom', cmdline_args=['electron/electron.exe', 'electron_app/main.js'], port=port)
-# eel.start('index.html', mode='custom', cmdline_args=['E:/python/MoeSR/electron/electron.exe', 'webui/main.js'], port=port)
-# py_run_process('RealESRGAN_x4plus', 256, 4, False, '', 'Image', 'test.png', 'output', '-1', 'real-esrgan')
-# py_run_process('x4_Anime_6B-Official', 64, 4, False, None, 'Image', 'input.jpg', '.', '0', 'real-esrgan')
 if __name__ == '__main__':
-    if os.getenv('production') == 'true':
+    if is_production:
         app.run(host='0.0.0.0', port=9000)
     else:
         app.run(debug=True)
