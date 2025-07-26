2cd9df0f4f20f4c362995ad21c76073a2d788f39
PuppyOne <zheng_xz@qq.com>
2025-07-26 23:26:44 +0800

    chore(client): update version display text
    - Change version string to `v0.0.1-nightly`

--- client/src/components/version.tsx
+++ client/src/components/version.tsx
@@ -4,7 +4,7 @@ import { Tooltip } from '@heroui/react';
 export default function Version() {
   return (
     <Tooltip content="别问为什么bug多，这个版本号你还不认识吗？" delay={500}>
-      v0.0.0-nightly-alpha-rc0-SNAPSHOT-UNSTABLE-DEV
+      v0.0.1-nightly-alpha-rc0-SNAPSHOT-UNSTABLE-DEV
     </Tooltip>
   );
 }
