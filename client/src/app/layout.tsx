735b8465555b0b5243755a8e67ba8a1d25d5dcb3
PuppyOne <zheng_xz@qq.com>
2025-07-28 16:24:26 +0800

    client: 调整全局布局

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,21 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Providers } from "./providers";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { Providers } from './providers';
+import Version from '@/components/version';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "不MoeSR",
-  description: "",
+  title: '不MoeSR',
+  description: '',
 };
 
 export default function RootLayout({
@@ -29,7 +30,14 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          {children}
+          <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+              {children}
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center text-xs text-gray-500">
+              <Version />
+            </footer>
+          </div>
         </Providers>
       </body>
     </html>
