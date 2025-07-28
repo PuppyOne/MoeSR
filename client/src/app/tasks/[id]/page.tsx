7c80cf9ebd683d917b3e71b1b0ee57ba59305ef1
PuppyOne <zheng_xz@qq.com>
2025-07-28 22:57:54 +0800

    client: 增加找不到 task 的错误处理 重定向到 404  避免服务器端错误

--- client/src/app/tasks/[id]/page.tsx
+++ client/src/app/tasks/[id]/page.tsx
@@ -1,4 +1,6 @@
 import { Button, Image, Link } from '@heroui/react';
+import { notFound } from 'next/navigation';
+import { HttpStatusCode, isAxiosError } from 'axios';
 import api from '@/lib/api';
 
 export default async function Task({
@@ -7,17 +9,25 @@ export default async function Task({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const { data } = await api.get<{ outputUrl: string }>(`/tasks/${id}`);
-  return (
-    <>
-      <Image
-        src={data.outputUrl}
-        alt="Processed Image"
-        className="w-full min-w-xs max-w-md"
-      />
-      <Button color="primary" as={Link} href={data.outputUrl} fullWidth>
-        Download
-      </Button>
-    </>
-  );
+  try {
+    const { data } = await api.get<{ outputUrl: string }>(`/tasks/${id}`);
+
+    return (
+      <>
+        <Image
+          src={data.outputUrl}
+          alt="Processed Image"
+          className="w-full min-w-xs max-w-md"
+        />
+        <Button color="primary" as={Link} href={data.outputUrl} fullWidth>
+          Download
+        </Button>
+      </>
+    );
+  } catch (error) {
+    if (isAxiosError(error) && error.status == HttpStatusCode.NotFound)
+      notFound();
+
+    throw error;
+  }
 }
